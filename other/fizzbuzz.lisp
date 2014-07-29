64a63cd2d4488ff0e9ce15dd5187657690bf13c2
Michal Skuza <michal@allocationerror.com>
2014-07-29 22:26:41 +0200

    Added let variables in fizzbuzz

--- other/fizzbuzz.lisp
+++ other/fizzbuzz.lisp
@@ -1,16 +1,18 @@
 (defun fizzbuzz ()
    (fizzbuzz-aux 1))
 
-(defun fizzbuzz-aux (N)   
-  (cond     
-    ((AND (zerop (rem N 3)) (zerop (rem N 5)))
-      (print 'fizzBuzz))   
-      ((zerop (rem N 3))
-   		(print 'Fizz))      
-   	((zerop (rem N 5))
-   		(print 'Buzz))      	
+(defun fizzbuzz-aux (N)     
+  (let ((mult3 (rem N 3))
+        (mult5 (rem N 5)))
+    (cond     
+       ((AND (zerop mult3) (zerop mult5))
+       (print 'fizzBuzz))   
+    ((zerop mult3)
+   		 (print 'Fizz))      
+    ((zerop mult5)
+   	   (print 'Buzz))      	
     (T (print N)))
 
   (if (< N 100)     	  
     (fizzbuzz-aux (+ N 1))
-    ()))
+    ())))
